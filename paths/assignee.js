fc478b9fd8c4c0bd7b64f028312a3d8a7cf69e5b
Ting Luo <tin831@gmail.com>
2019-11-05 13:08:44 -0800

    added pr and assign js

--- paths/assignee.js
+++ paths/assignee.js
@@ -2,29 +2,31 @@
 
 const superagent = require('superagent');
 
-function getAssignees (request, response) {
+function getAssignees(request, response) {
 
   let url = `https://api.github.com/orgs/C-T-R-L-Z/issues?filter=all&status=all`;
 
   superagent.get(url)
     .set('User-Agent', 'C-T-R-L-Z')
-    .auth (process.env.username, process.env.password)
-    .then (results => {
+    .auth(process.env.username, process.env.password)
+    .then(results => {
       let issuesArr = results.body
-      let assignees = [];
+      let assigneeResults = []
       issuesArr.forEach(issue => {
-        let found = false;
-        assignees.forEach(person=> {
-          if (issue.assignees.login === person.name) {
-            person.assignee++;
-            found = true;
+        issue.assignees.forEach(assignee => {
+          let found = false
+          assigneeResults.forEach(person => {
+            if (assignee.login === person.name) {
+              person.assigned++;
+              found = true;
+            }
+          })
+          if (!found) {
+            assigneeResults.push({ name: assignee.login, assigned: 1 })
           }
         })
-        if (!found) {
-          assignees.push({name:issue.assignees.login, assignedIssue:1})
-        }
-      });
-      console.log(assignees)
+      })
+      console.log(assigneeResults)
     })
     .catch(err => console.error(err));
 }
