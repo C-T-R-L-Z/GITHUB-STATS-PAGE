4e31e713a2516bf9aa3f2bde9213682a7843be7b
Ting Luo <tin831@gmail.com>
2019-11-04 17:35:16 -0800

    added assignee.js

--- paths/assignee.js
+++ paths/assignee.js
@@ -2,7 +2,7 @@
 
 const superagent = require('superagent');
 
-function getIssues (request, response) {
+function getAssignees (request, response) {
 
   let url = `https://api.github.com/orgs/C-T-R-L-Z/issues?filter=all&status=all`;
 
@@ -11,22 +11,22 @@ function getIssues (request, response) {
     .auth (process.env.username, process.env.password)
     .then (results => {
       let issuesArr = results.body
-      let openIssues = [];
+      let assignees = [];
       issuesArr.forEach(issue => {
         let found = false;
-        openIssues.forEach(person=> {
-          if (issue.user.login === person.name) {
-            person.issue++;
+        assignees.forEach(person=> {
+          if (issue.assignees.login === person.name) {
+            person.assignee++;
             found = true;
           }
         })
         if (!found) {
-          openIssues.push({name:issue.user.login, issue:1})
+          assignees.push({name:issue.assignees.login, assignedIssue:1})
         }
       });
-      console.log(openIssues)
+      console.log(assignees)
     })
     .catch(err => console.error(err));
 }
 
-module.exports = getIssues;
+module.exports = getAssignees;
