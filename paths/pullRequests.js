2eef380d6f0a1f0a4dfa60bdc34eb05effdba65f
Christopher Stanley <CStanley4848@gmail.com>
2019-11-05 17:30:34 -0800

    got more data to play with

--- paths/pullRequests.js
+++ paths/pullRequests.js
@@ -2,20 +2,49 @@
 
 const superagent = require('superagent');
 
-function findPR(request, response, orgData) {
+function findPR(orgData) {
 
   let url = `https://api.github.com/orgs/C-T-R-L-Z/repos`;
 
-  superagent.get(url)
+  return superagent.get(url)
     .set('User-Agent', 'C-T-R-L-Z')
     .auth(process.env.username, process.env.password)
-    .then(results => {
-      console.log(results.body);
-      // let repoName = results.body.map()
-    });
+    .then(reposList => {
+
+      let repos = reposList.body;
+
+      let repoCalls = repos.map(repo =>{
+        let url = `https://api.github.com/repos/C-T-R-L-Z/GITHUB-STATS-PAGE/pulls?state=all`;
+
+        return superagent.get(url)
+          .set('User-Agent', 'C-T-R-L-Z')
+          .auth(process.env.username, process.env.password);
+
+      });
+
+      return Promise.all(repoCalls)
+        .then(results => {
+          results.forEach(repo => {
+
+            orgData.totalPulls += repo.body.length;
+            repo.body.forEach(pullRequest => {
+
+              orgData.members.forEach(person => {
+
+                if (person.name === pullRequest.user.login) {
+                  person.pulls++;
+                }
+
+              });
+            });
+          });
+
+          return orgData;
+
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
 }
 
-
-
 module.exports = findPR;
-
