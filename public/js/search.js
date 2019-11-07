e0a3c3c64bbd6fb3837945d72552f99c7aee7dbd
bond <ilkerzaimoglu@gmail.com>
2019-11-06 17:08:39 -0800

    about graphs

--- public/js/search.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-$('button').on('click', whichButton);
-
-function whichButton (event) {
-  console.log(this.value);
-  // let names = {orgName: this.value,username: $('input')[0].value, };
-  
-}
