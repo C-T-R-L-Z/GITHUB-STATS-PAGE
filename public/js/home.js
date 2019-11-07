19430cb6e2bd7ee1a644cea07bbeb72042513388
Ting Luo <tin831@gmail.com>
2019-11-07 13:27:34 -0800

    added instruction for personal token

--- public/js/home.js
+++ public/js/home.js
@@ -8,13 +8,23 @@ function showForm(event) {
   $('#keyForm').toggle();
 }
 
-function createCookie (event) {
+function createCookie(event) {
   const entries = $('#keyForm input');
   document.cookie = `username=${entries[0].value};`;
   document.cookie = `key=${entries[1].value};`;
 }
 
-function removeCookies () {
+function removeCookies() {
   document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
   document.cookie = 'key=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 }
+
+$('#show').on('click', function () {
+  $('#ptag').toggle();
+});
+
+$('#atag').on('click', function (e) {
+  e.preventDefault();
+  var url = $(this).attr('href');
+  window.open(url, '_blank');
+});
